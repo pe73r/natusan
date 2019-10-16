5974e149552c41c4dffa08fc81a0abbd62e09a09
Pedro <pedromatanzo@gmail.com>
2019-10-16 12:25:27 +0200

    two sections rondorff

--- src/scripts/vendor.js
+++ src/scripts/vendor.js
@@ -8,6 +8,12 @@
  */
 // =require vendor/jquery-2.2.3.min.js
 
+/*!
+ * lazyloading.js
+ */
+// =require vendor/lazyloading.js
+
+
 // Attempts to preserve comments that likely contain licensing information,
 // even if the comment does not have directives such as `@license` or `/*!`.
 //
